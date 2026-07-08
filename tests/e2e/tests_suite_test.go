3d0438edc934fbd85f4c77a812e2e6cb898d6b67
Enrico Candino <enrico.candino@suse.com>
2026-07-08 15:58:06 +0200

    Update k8s dependencies to v1.36 (main) (#979)
    * Update k3s version in test suites to v1.36.2-k3s1
    
    - Updated the k3sVersion and k3sOldVersion constants in both cli and e2e test suites to reflect the new version v1.36.2-k3s1 and the previous version v1.36.0-k3s1.
    
    * updated README.md

--- tests/e2e/tests_suite_test.go
+++ tests/e2e/tests_suite_test.go
@@ -37,8 +37,8 @@ import (
 const (
 	k3kNamespace = "k3k-system"
 
-	k3sVersion    = "v1.35.2-k3s1"
-	k3sOldVersion = "v1.35.0-k3s1"
+	k3sVersion    = "v1.36.2-k3s1"
+	k3sOldVersion = "v1.36.0-k3s1"
 
 	e2eTestLabel           = "e2e"
 	slowTestsLabel         = "slow"
